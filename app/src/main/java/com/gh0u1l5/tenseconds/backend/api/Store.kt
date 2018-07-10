c2177f46e9a24bbe7df1418a4ec38836a09f35c3
Gh0u1L5 <Em3rs0n@yahoo.com>
2018-07-10 19:37:36 -0400

    Fix TimeStamp problem caused by Date.

--- app/src/main/java/com/gh0u1l5/tenseconds/backend/api/Store.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/backend/api/Store.kt
@@ -8,11 +8,16 @@ import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 @Suppress("MemberVisibilityCanBePrivate")
 object Store {
     val instance by lazy {
-        FirebaseFirestore.getInstance()
+        FirebaseFirestore.getInstance().apply {
+            firestoreSettings = FirebaseFirestoreSettings.Builder()
+                    .setTimestampsInSnapshotsEnabled(true)
+                    .build()
+        }
     }
 
     object IdentityCollection {
