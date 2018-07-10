4132ff2b59afb46c8862dd97a9fadef29cfbc27e
Gh0u1L5 <Em3rs0n@yahoo.com>
2018-07-10 16:09:46 -0400

    Add more functions and tests in CryptoUtils.

--- app/src/main/java/com/gh0u1l5/tenseconds/backend/crypto/CryptoUtils.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/backend/crypto/CryptoUtils.kt
@@ -1,5 +1,8 @@
 package com.gh0u1l5.tenseconds.backend.crypto
 
+import java.security.MessageDigest
+import javax.crypto.spec.SecretKeySpec
+
 object CryptoUtils {
     fun ByteArray.erase() {
         this.fill(0)
@@ -9,7 +12,27 @@ object CryptoUtils {
         this.fill('\u0000')
     }
 
-    fun ByteArray.toHexString(): String {
-        return this.joinToString { String.format("%02X", it) }
+    /**
+     * Erases a raw secret key using reflection.
+     */
+    fun SecretKeySpec.erase() {
+        val keyField = this.javaClass.getDeclaredField("key").apply {
+            isAccessible = true
+        }
+        (keyField.get(this) as ByteArray).erase()
+    }
+
+    fun ByteArray.toSHA256(): ByteArray {
+        return MessageDigest.getInstance("SHA-256").digest(this)
+    }
+
+    fun ByteArray.fromBytesToHexString(): String {
+        return this.joinToString("") { String.format("%02X", it) }
+    }
+
+    fun String.fromHexStringToBytes(): ByteArray {
+        return ByteArray(this.length / 2) {
+            this.substring(it * 2, it * 2 + 2).toInt(16).toByte()
+        }
     }
 }
---
+++ app/src/test/java/com/gh0u1l5/tenseconds/backend/crypto/CryptoUtilsTests.kt
@@ -0,0 +1,40 @@
+package com.gh0u1l5.tenseconds.backend.crypto
+
+import org.junit.Test
+
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.erase
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.toSHA256
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.fromBytesToHexString
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.fromHexStringToBytes
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
+
+class CryptoUtilsTests {
+    @Test fun testErase() {
+        val bytes = "1111".toByteArray()
+        val chars = "1111".toCharArray()
+        bytes.erase()
+        assertArrayEquals(byteArrayOf(0, 0, 0, 0), bytes)
+        chars.erase()
+        assertArrayEquals(charArrayOf('\u0000', '\u0000', '\u0000', '\u0000'), chars)
+    }
+
+    @Test fun testSHA256() {
+        val caseA = "abcdefg".toByteArray() to "7D1A54127B222502F5B79B5FB0803061152A44F92B37E23C6527BAF665D4DA9A"
+        val caseB = "1234567".toByteArray() to "8BB0CF6EB9B17D0F7D22B456F121257DC1254E1F01665370476383EA776DF414"
+        assertEquals(caseA.first.toSHA256().fromBytesToHexString(), caseA.second)
+        assertEquals(caseB.first.toSHA256().fromBytesToHexString(), caseB.second)
+    }
+
+    @Test fun testFromBytesToHexString() {
+        assertEquals("", byteArrayOf().fromBytesToHexString())
+        assertEquals("0102030405060708090A0B0C0D0E0F", byteArrayOf(0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F).fromBytesToHexString())
+        assertEquals("F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF", byteArrayOf(-0x0F, -0x0E, -0x0D, -0x0C, -0x0B, -0x0A, -0x09, -0x08, -0x07, -0x06, -0x05, -0x04, -0x03, -0x02, -0x01).fromBytesToHexString())
+    }
+
+    @Test fun testFromHexStringToBytes() {
+        assertArrayEquals(byteArrayOf(), "".fromHexStringToBytes())
+        assertArrayEquals(byteArrayOf(0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F), "0102030405060708090A0B0C0D0E0F".fromHexStringToBytes())
+        assertArrayEquals(byteArrayOf(-0x0F, -0x0E, -0x0D, -0x0C, -0x0B, -0x0A, -0x09, -0x08, -0x07, -0x06, -0x05, -0x04, -0x03, -0x02, -0x01), "F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF".fromHexStringToBytes())
+    }
+}
