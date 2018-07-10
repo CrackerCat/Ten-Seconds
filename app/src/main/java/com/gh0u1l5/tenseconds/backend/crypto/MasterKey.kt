b3a4a547ac76cf0a0c4270307bfec22f07ba0386
Gh0u1L5 <Em3rs0n@yahoo.com>
2018-07-10 16:11:54 -0400

    Finish the cryptographic logic about master keys.

--- app/src/main/java/com/gh0u1l5/tenseconds/backend/crypto/MasterKey.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/backend/crypto/MasterKey.kt
@@ -1,13 +1,15 @@
 package com.gh0u1l5.tenseconds.backend.crypto
 
 import android.content.Context
-
-import com.gh0u1l5.tenseconds.global.Constants.PBKDF2_ITERATIONS
+import com.gh0u1l5.tenseconds.backend.api.Store
 import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.erase
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.fromBytesToHexString
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.fromHexStringToBytes
+import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.toSHA256
+import com.gh0u1l5.tenseconds.global.Constants.PBKDF2_ITERATIONS
+import com.gh0u1l5.tenseconds.global.Constants.PREF_NAME_MASTER_KEYS
 import com.gh0u1l5.tenseconds.global.TenSecondsApplication
-import com.gh0u1l5.tenseconds.backend.crypto.CryptoUtils.toHexString
 import javax.crypto.SecretKey
-
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 import javax.crypto.spec.SecretKeySpec
@@ -27,40 +29,78 @@ object MasterKey {
     }
 
     /**
-     * Derives a AES-256 master key from a passphrase using PBKDF2.
+     * Derives an AES-256 master key from a passphrase using PBKDF2.
      *
      * @param identityId The ID of the identity which owns this master key. This ID will be used as
      * salt value in PBKDF2.
      * @param passphrase The passphrase used to derive the AES-256 key. Notice that after this
-     * derivation, the passphrase will be erased immediately.
+     * operation, this passphrase will be erased immediately.
      */
-    private fun derive(identityId: String, passphrase: CharArray): ByteArray {
-        val salt = identityId.toByteArray()
+    private fun derive(identityId: String, passphrase: CharArray): SecretKey {
+        var buffer: ByteArray? = null
         try {
-            return sPBEKeyFactory.generateSecret(
-                    PBEKeySpec(passphrase, salt, PBKDF2_ITERATIONS, 32 * 8)
-            ).encoded
+            val salt = identityId.toByteArray()
+            val spec = PBEKeySpec(passphrase, salt, PBKDF2_ITERATIONS, 32 * 8)
+            buffer = sPBEKeyFactory.generateSecret(spec).encoded
+            return SecretKeySpec(buffer, "AES")
         } finally {
+            buffer?.erase()
             passphrase.erase()
         }
     }
 
-    fun generate(identityId: String, passphrase: CharArray): SecretKey {
-        val key = SecretKeySpec(derive(identityId, passphrase), "AES")
+    /**
+     * Updates the passphrase bounded to a specified identity. It will
+     *   1. Generates an AES-256 master key based on the given passphrase.
+     *   2. Encrypts this key using the root key and stores the encrypted value locally.
+     *   3. Stores the hash SHA256(identityId + SHA256(key)) to FireStore.
+     *
+     * @param identityId The ID of the identity which owns this master key.
+     * @param passphrase The passphrase used to derive the AES-256 key. Notice that after this
+     * operation, this passphrase will be erased immediately.
+     */
+    fun update(identityId: String, passphrase: CharArray) {
+        val key = derive(identityId, passphrase)
+        try {
+            // Update local storage
+            val iv = ByteArray(12)
+            val wrappedKey = RootKey.wrap(key, iv)
+            val context = TenSecondsApplication.instance
+            context.getSharedPreferences(PREF_NAME_MASTER_KEYS, Context.MODE_PRIVATE)
+                    .edit()
+                    .putString("${identityId}_iv", iv.fromBytesToHexString())
+                    .putString("${identityId}_key", wrappedKey.fromBytesToHexString())
+                    .apply()
 
-        val iv = ByteArray(12)
-        val wrappedKey = RootKey.wrap(key, iv)
-
-        TenSecondsApplication.instance.getSharedPreferences("master_keys", Context.MODE_PRIVATE)
-                .edit()
-                .putString("${identityId}_iv", iv.toHexString())
-                .putString("${identityId}_key", wrappedKey.toHexString())
-                .apply()
-
-        return key
+            // Update remote storage
+            val hash = (identityId.toByteArray() + key.encoded.toSHA256()).toSHA256()
+            val data = mapOf("master" to hash.fromBytesToHexString())
+            Store.IdentityCollection.update(identityId, data)
+        } finally {
+            (key as SecretKeySpec).erase()
+        }
     }
 
-    fun retrieve(identityId: String, passphrase: CharArray) {
+    /**
+     * Retrieves the AES-256 master key of the specified identity. Remember, it is caller's
+     * responsibility to erase the [SecretKey] object.
+     *
+     * @param identityId The ID of the identity which owns this master key.
+     */
+    private fun retrieve(identityId: String): SecretKey? {
+        // TODO: handle fingerprint
 
+        val context = TenSecondsApplication.instance
+        val masterKeys = context.getSharedPreferences(PREF_NAME_MASTER_KEYS, Context.MODE_PRIVATE)
+
+        val ivHex = masterKeys.getString("${identityId}_iv", null)
+        val wrappedKeyHex = masterKeys.getString("${identityId}_key", null)
+        if (ivHex == null || wrappedKeyHex == null) {
+            return null
+        }
+
+        val iv = ivHex.fromHexStringToBytes()
+        val wrappedKey = wrappedKeyHex.fromHexStringToBytes()
+        return RootKey.unwrap(wrappedKey, iv) as SecretKey
     }
 }
--- app/src/main/java/com/gh0u1l5/tenseconds/global/Constants.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/global/Constants.kt
@@ -2,4 +2,6 @@ package com.gh0u1l5.tenseconds.global
 
 object Constants {
     const val PBKDF2_ITERATIONS = 10000
+
+    const val PREF_NAME_MASTER_KEYS = "master_keys"
 }
