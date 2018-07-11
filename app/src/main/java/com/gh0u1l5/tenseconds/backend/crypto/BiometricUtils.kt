7ce1c8f4d9e8abdacae50886ecf4114335468c67
Em3rs0n <Em3rs0n@yahoo.com>
2018-07-11 03:17:10 -0400

    Add authenticate function in BiometricUtils.

--- app/src/main/java/com/gh0u1l5/tenseconds/backend/crypto/BiometricUtils.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/backend/crypto/BiometricUtils.kt
@@ -1,9 +1,16 @@
 package com.gh0u1l5.tenseconds.backend.crypto
 
+import android.content.DialogInterface
 import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
 import android.hardware.fingerprint.FingerprintManager
+import android.os.AsyncTask
 import android.os.Build
+import android.support.annotation.RequiresApi
+import android.widget.Toast
+import com.gh0u1l5.tenseconds.R
 import com.gh0u1l5.tenseconds.global.TenSecondsApplication
+import javax.crypto.Cipher
 
 object BiometricUtils {
     private val context by lazy { TenSecondsApplication.instance }
@@ -25,6 +32,42 @@ object BiometricUtils {
         } else {
             val fm = context.getSystemService(FingerprintManager::class.java)
             fm.hasEnrolledFingerprints()
+        }
+    }
+
+    fun authenticate(cipher: Cipher, success: (Cipher) -> Unit) {
+        if (Build.VERSION.SDK_INT >= 28) {
+            val title = context.getString(R.string.biometric_prompt_title)
+            val cancel = context.getString(R.string.button_cancel)
+            val crypto = BiometricPrompt.CryptoObject(cipher)
+            val executor = AsyncTask.THREAD_POOL_EXECUTOR
+            BiometricPrompt.Builder(context)
+                    .setTitle(title)
+                    .setNegativeButton(cancel, executor, DialogInterface.OnClickListener { dialog, _ ->
+                        dialog.dismiss()
+                    })
+                    .build()
+                    .authenticate(crypto, null, executor, object : BiometricPrompt.AuthenticationCallback() {
+                        @RequiresApi(28)
+                        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                            success(result.cryptoObject.cipher)
+                        }
+                        override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                            Toast.makeText(context, errString, Toast.LENGTH_LONG).show()
+                        }
+                    })
+        } else {
+            // TODO: add a dialog for this one
+            val fm = context.getSystemService(FingerprintManager::class.java)
+            val crypto = FingerprintManager.CryptoObject(cipher)
+            fm.authenticate(crypto, null, 0, object : FingerprintManager.AuthenticationCallback() {
+                override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult) {
+                    success(result.cryptoObject.cipher)
+                }
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                    Toast.makeText(context, errString, Toast.LENGTH_LONG).show()
+                }
+            }, null)
         }
     }
 }
