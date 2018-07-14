0f8a64f6190ae868fc840bde40ea23dd2284b642
Em3rs0n <Em3rs0n@yahoo.com>
2018-07-13 23:57:55 -0400

    Improve the default constructor for PasswordSpec.

--- app/src/main/java/com/gh0u1l5/tenseconds/backend/bean/PasswordSpec.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/backend/bean/PasswordSpec.kt
@@ -1,5 +1,6 @@
 package com.gh0u1l5.tenseconds.backend.bean
 
+import com.gh0u1l5.tenseconds.global.CharType
 import com.gh0u1l5.tenseconds.global.CharType.LOWER_LETTERS
 import com.gh0u1l5.tenseconds.global.CharType.NUMBERS
 import com.gh0u1l5.tenseconds.global.CharType.SYMBOLS
@@ -13,6 +14,6 @@ import com.gh0u1l5.tenseconds.global.CharType.UPPER_LETTERS
  * [SYMBOLS], [LOWER_LETTERS] or [UPPER_LETTERS].
  */
 data class PasswordSpec (
-        var length: Int = 0,
-        var types: Int = 0
+        var length: Int = 16,
+        var types: Int = CharType.ALL
 )
