be8e1f4ff85e25ff7eebed24c79ddd654ab54c5e
Em3rs0n <Em3rs0n@yahoo.com>
2018-07-16 19:07:36 -0400

    Remove useless codes

--- app/src/main/java/com/gh0u1l5/tenseconds/backend/api/Auth.kt
+++ app/src/main/java/com/gh0u1l5/tenseconds/backend/api/Auth.kt
@@ -1,15 +1,11 @@
 package com.gh0u1l5.tenseconds.backend.api
 
 import android.app.Activity
-import com.gh0u1l5.tenseconds.backend.api.TaskDecorators.withFailureLog
 import com.gh0u1l5.tenseconds.backend.bean.OAuthInfo
 import com.gh0u1l5.tenseconds.global.Constants.RC_GOOGLE_SIGN_IN
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-
 
 @Suppress("MemberVisibilityCanBePrivate")
 object Auth {
@@ -22,26 +18,6 @@ object Auth {
         GitHub,
         Google,
         Twitter,
-    }
-
-    /**
-     * true if a login attempt is in progress, false otherwise
-     */
-    @Volatile var isLoggingIn = false
-        private set
-
-    /**
-     * Try to login into Firebase using tryLogin callback.
-     */
-    fun login(tryLogin: FirebaseAuth.() -> Task<AuthResult>): Task<AuthResult>? {
-        if (isLoggingIn) {
-            return null
-        }
-        isLoggingIn = true
-
-        return instance.tryLogin()
-                .addOnCompleteListener { isLoggingIn = false }
-                .withFailureLog("FireAuth")
     }
 
     private fun retrieveGoogleOAuthToken(activity: Activity, info: OAuthInfo) {
